f9f5c448012e073400a45194fcaccad882e55f20
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-09-07 14:35:38 +0100

    commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,60 @@
+function Home() {
+    return (
+      <div className="Home">
+          <div className="w-1440 h-1327 relative bg-green-100">
+              <div className="w-1337 h-642.68 absolute left-103 top-148">
+                  <div className="w-192.55 h-192.55 absolute left-744.32 top-124.79 bg-gray-500 rounded-full"></div>
+                  <div className="w-250.88 h-250.88 absolute left-933.18 top-76.65 bg-gray-700 rounded-full"></div>
+                  <div className="w-143.49 h-143.49 absolute left-682.30 top-471.02 bg-gray-700 rounded-full"></div>
+                  <div className="w-245.32 h-245.32 absolute left-935.95 top-369.19 bg-slate-800 rounded-full"></div>
+                  <div className="w-191.63 h-192.55 absolute left-1056.30 top-255.32 bg-slate-300 rounded-full"></div>
+                  <div className="w-198.11 h-198.11 absolute left-700.82 top-326.60 bg-gray-600 rounded-full"></div>
+                  <div className="w-1337 h-642.68 absolute left-0 top-0">
+                      <div className="w-32.65 h-32.65 absolute left-388.36 top-30.07 bg-white rounded-full"></div>
+                      <div className="w-32.65 h-32.65 absolute left-922.77 top-208.78 bg-white rounded-full"></div>
+                      <div className="w-32.65 h-32.65 absolute left-725.16 top-265.49 bg-white rounded-full"></div>
+                      <div className="w-32.65 h-32.65 absolute left-161.53 top-287.83 bg-white rounded-full"></div>
+                      <div className="w-32.65 h-32.65 absolute left-1101.49 top-451.08 bg-white rounded-full"></div>
+                  </div>
+              </div>
+              <img className="w-653 h-653 absolute left-759 top-182" src="https://via.placeholder.com/653x653" alt="Placeholder" />
+              <div className="w-387 h-34 absolute left-612 top-60">
+                  <div className="w-120 h-34 absolute">
+                      <div className="w-59 h-0.5 absolute left-31 top-32 bg-green-700 rounded-15"></div>
+                      <div className="w-120 h-27.85 absolute text-center text-green-700 text-lg font-bold">Home</div>
+                  </div>
+                  <div className="w-120 h-27.85 absolute left-127 top-0 text-center text-black text-lg font-medium">About</div>
+                  <div className="w-120 h-27.85 absolute left-267 top-0 text-center text-black text-lg font-medium">Contact</div>
+              </div>
+              <div className="w-266.33 h-50.63 absolute left-1146 top-49">
+                  <div className="w-126.58 h-50.63 absolute">
+                      <div className="w-126.58 h-50.63 absolute rounded-md border border-neutral-400"></div>
+                      <div className="w-70.89 h-21.52 absolute left-27.85 top-13.92 text-center text-black text-sm font-semibold">Sign up</div>
+                  </div>
+                  <div className="w-126.58 h-50.63 absolute left-139.74 top-0">
+                      <div className="w-126.58 h-50.63 absolute bg-gradient-to-r from-green-600 to-green-700 rounded-9 border border-white"></div>
+                      <div className="w-58.23 h-21.52 absolute left-34.18 top-13.92 text-center text-white text-sm font-bold">Log in</div>
+                  </div>
+              </div>
+              <div className="w-264 h-45.88 absolute left-67 top-51"></div>
+              <div className="w-991 h-606 absolute left-67 top-205">
+                  <div className="w-991 h-266 absolute">
+                      <span className="text-zinc-900 text-65 font-bold leading-82">Don't Miss Out On <br />The </span>
+                      <span className="text-green-700 text-65 font-bold leading-82">Important <br />Things</span>
+                      <span className="text-zinc-900 text-65 font-bold leading-82">.</span>
+                  </div>
+                  <div className="w-545 h-227 absolute left-0 top-282">
+                      <span className="text-black text-xl font-normal leading-37">Do you or someone you know have a habit of forgetting stuff? Don't worry!!! here's </span>
+                      <span className="text-green-700 text-xl font-bold leading-37">Reminder Me</span>
+                      <span className="text-green-700 text-xl font-normal leading-37"> </span>
+                      <span className="text-black text-xl font-normal leading-37">to the rescue. Reminder Me is a reminder management application that lets you send reminders through text message.</span>
+                  </div>
+                  <div className="w-297 h-84 absolute left-0 top-522 bg-gradient-to-r from-green-600 to-green-700 rounded-9 border border-white"></div>
+                  <div className="w-104.44 h-38.40 absolute left-96.30 top-544.48 text-center text-white text-lg font-bold">Download Now</div>
+              </div>
+          </div>
+      </div>
+    );
+  }
+  
+  export default Home;
