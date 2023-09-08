2a261544909e41153eb84b0401aab2f0530e3ff2
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-09-08 02:15:31 +0100

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,93 +1,18 @@
 function Home() {
     return (
       <div className="Home">
-        <div className="w-[1440px] h-[1327px] relative bg-green-100">
-    <div className="w-[1337px] h-[642.68px] left-[103px] top-[148px] absolute">
-        <div className="w-[192.55px] h-[192.55px] left-[744.32px] top-[124.79px] absolute bg-gray-500 rounded-full" />
-        <div className="w-[250.88px] h-[250.88px] left-[933.18px] top-[76.65px] absolute bg-gray-700 rounded-full" />
-        <div className="w-[143.49px] h-[143.49px] left-[682.30px] top-[471.02px] absolute bg-gray-700 rounded-full" />
-        <div className="w-[245.32px] h-[245.32px] left-[935.95px] top-[369.19px] absolute bg-slate-800 rounded-full" />
-        <div className="w-[191.63px] h-[192.55px] left-[1056.30px] top-[255.32px] absolute bg-slate-300 rounded-full" />
-        <div className="w-[198.11px] h-[198.11px] left-[700.82px] top-[326.60px] absolute bg-gray-600 rounded-full" />
-        <div className="w-[1337px] h-[642.68px] left-0 top-0 absolute">
-            <div className="w-[32.65px] h-[32.65px] left-[388.36px] top-[30.07px] absolute bg-white rounded-full" />
-            <div className="w-[32.65px] h-[32.65px] left-[922.77px] top-[208.78px] absolute bg-white rounded-full" />
-            <div className="w-[32.65px] h-[32.65px] left-[725.16px] top-[265.49px] absolute bg-white rounded-full" />
-            <div className="w-[32.65px] h-[32.65px] left-[161.53px] top-[287.83px] absolute bg-white rounded-full" />
-            <div className="w-[32.65px] h-[32.65px] left-[1101.49px] top-[451.08px] absolute bg-white rounded-full" />
+        <div className="w-[991px] h-[266px]">
+            <span className="text-zinc-900 text-[65px] font-bold leading-[82px]">Don't Miss Out On <br/>The </span>
+            <span className="text-green-700 text-[65px] font-bold leading-[82px]">Important <br/>Things</span>
+            <span className="text-zinc-900 text-[65px] font-bold leading-[82px]">.</span>
         </div>
-    </div>
-    <img className="w-[653px] h-[653px] left-[759px] top-[182px] absolute" src="https://via.placeholder.com/653x653" />
-    <div className="w-[387px] h-[34px] left-[612px] top-[60px] absolute">
-        <div className="w-[120px] h-[34px] left-0 top-0 absolute">
-            <div className="w-[59px] h-0.5 left-[31px] top-[32px] absolute bg-green-700 rounded-[15px]" />
-            <div className="w-[120px] h-[27.85px] left-0 top-0 absolute text-center text-green-700 text-lg font-bold">Home</div>
+        <div className="w-[545px] h-[227px]">
+            <span className="text-black text-xl font-normal leading-[37px]">Do you or someone you know have a habit of forgetting stuff? Don't worry!!! here's </span>
+            <span className="text-green-700 text-xl font-bold leading-[37px]">Reminder Me</span>
+            <span className="text-green-700 text-xl font-normal leading-[37px]"> </span>
+            <span className="text-black text-xl font-normal leading-[37px]">to the rescue. Reminder Me is a reminder management application that lets you send reminders through text message.</span>
         </div>
-        <div className="w-[120px] h-[27.85px] left-[127px] top-0 absolute text-center text-black text-lg font-medium">About</div>
-        <div className="w-[120px] h-[27.85px] left-[267px] top-0 absolute text-center text-black text-lg font-medium">Contact</div>
-    </div>
-    <div className="w-[266.33px] h-[50.63px] left-[1146px] top-[49px] absolute">
-        <div className="w-[126.58px] h-[50.63px] left-0 top-0 absolute">
-            <div className="w-[126.58px] h-[50.63px] left-0 top-0 absolute rounded-md border border-neutral-400" />
-            <div className="w-[70.89px] h-[21.52px] left-[27.85px] top-[13.92px] absolute text-center text-black text-sm font-semibold">Sign up</div>
-        </div>
-        <div className="w-[126.58px] h-[50.63px] left-[139.74px] top-0 absolute">
-            <div className="w-[126.58px] h-[50.63px] left-0 top-0 absolute bg-gradient-to-r from-green-600 to-green-700 rounded-[9px] border border-white" />
-            <div className="w-[58.23px] h-[21.52px] left-[34.18px] top-[13.92px] absolute text-center text-white text-sm font-bold">Log in</div>
-        </div>
-    </div>
-    <div className="w-[264px] h-[45.88px] left-[67px] top-[51px] absolute">
-    </div>
-    <div className="w-[991px] h-[606px] left-[67px] top-[205px] absolute">
-        <div className="w-[991px] h-[266px] left-0 top-0 absolute"><span className="text-zinc-900 text-[65px] font-bold leading-[82px]">Don't Miss Out On <br/>The </span>
-<span className="text-green-700 text-[65px] font-bold leading-[82px]">Important <br/>Things</span>
-<span className="text-zinc-900 text-[65px] font-bold leading-[82px]">.</span>
-</div>
-        <div className="w-[545px] h-[227px] left-0 top-[282px] absolute"><span className="text-black text-xl font-normal leading-[37px]">Do you or someone you know have a habit of forgetting stuff? Don't worry!!! here's </span>
-<span className="text-green-700 text-xl font-bold leading-[37px]">Reminder Me</span>
-<span className="text-green-700 text-xl font-normal leading-[37px]"> </span>
-<span className="text-black text-xl font-normal leading-[37px]">to the rescue. Reminder Me is a reminder management application that lets you send reminders through text message.</span>
-</div>
-        <div className="w-[297px] h-[77px] left-0 top-[529px] absolute">
-            <div className="w-[297px] h-[77px] left-0 top-0 absolute bg-gradient-to-r from-green-600 to-green-700 rounded-xl shadow border border-white" />
-            <div className="w-[161.63px] h-[25.20px] left-[67.78px] top-[26.07px] absolute text-center text-neutral-50 text-2xl font-medium leading-7">Get Started</div>
-        </div>
-    </div>
-    <div className="w-[410px] h-[273px] left-[67px] top-[917px] absolute bg-zinc-100 bg-opacity-95 rounded-[10px] shadow-inner">
-        <div className="w-[43px] h-[43px] left-[65.88px] top-[68.88px] absolute bg-sky-200 rounded-full" />
-        <div className="left-[47px] top-[133.88px] absolute text-zinc-800 text-[23px] font-medium capitalize leading-[30.64px]">Schedule Messages with Ease</div>
-        <div className="w-[343px] left-[47px] top-[179px] absolute text-neutral-600 text-base font-normal capitalize leading-relaxed">Schedule important messages up to 7 days ahead effortlessly with our user-friendly reminder application.</div>
-        <div className="w-[47.31px] h-[57.41px] left-[41.62px] top-[50px] absolute">
-        </div>
-    </div>
-    <div className="w-[410px] h-[273px] left-[517px] top-[917px] absolute bg-zinc-100 bg-opacity-95 rounded-[10px] shadow-inner">
-        <div className="w-[43px] h-[43px] left-[65.88px] top-[68.88px] absolute bg-sky-200 rounded-full" />
-        <div className="left-[47px] top-[133.88px] absolute text-zinc-800 text-[23px] font-medium capitalize leading-[30.64px]">Efficient Reminder Management</div>
-        <div className="w-[343px] left-[47px] top-[179px] absolute text-neutral-600 text-base font-normal capitalize leading-relaxed">Manage your reminders efficiently to ensure you or love ones never miss a beat.</div>
-        <div className="w-[57px] h-[57px] left-[37px] top-[50px] absolute" />
-    </div>
-    <div className="w-[410px] h-[273px] left-[967px] top-[917px] absolute bg-zinc-100 bg-opacity-95 rounded-[10px] shadow-inner">
-        <div className="w-[43px] h-[43px] left-[65.88px] top-[68.88px] absolute bg-sky-200 rounded-full" />
-        <div className="w-[55px] h-[55px] left-[35px] top-[52px] absolute">
-        </div>
-        <div className="left-[47px] top-[133.88px] absolute text-zinc-800 text-[23px] font-medium capitalize leading-[30.64px]">Provided Phone Number</div>
-        <div className="w-[343px] left-[47px] top-[179px] absolute text-neutral-600 text-base font-normal capitalize leading-relaxed">Stay connected with ease with provided phone number for timely notifications</div>
-    </div>
-    <div className="pl-[67px] pr-[63px] py-[25px] left-0 top-[1237px] absolute bg-green-700 justify-end items-start gap-[937px] inline-flex">
-        <div className="self-stretch justify-start items-center gap-2.5 inline-flex">
-            <div className="w-10 h-10 relative shadow" />
-            <div className="text-white text-xl font-bold">REMINDER ME</div>
-        </div>
-        <div className="w-[182px] justify-center items-start gap-10 inline-flex">
-            <div className="w-[34px] h-[34px] relative flex-col justify-start items-start flex">
-                <div className="w-[34px] h-[34px] relative">
-                </div>
-            </div>
-            <div className="w-[34px] h-[34px] relative flex-col justify-start items-start flex" />
-            <div className="w-[34px] h-[34px] relative flex-col justify-start items-start flex" />
-        </div>
-    </div>
-</div>
+        
       </div>
     );
   }
