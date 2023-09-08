84111a2b68910ea11895af1647eaaabf4d6810f5
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-09-08 17:43:12 +0100

    commit

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -1,25 +1,53 @@
 import React from 'react';
 import logo from '../images/logo.png';
+import ReminderMelogo from '../images/reminderMe.png';
 
 function App() {
   const [isOpen, setIsOpen] = React.useState(false);
+  const [activePage, setActivePage] = React.useState('Home'); // For demo purposes
 
   return (
-    <div style={{ backgroundColor: '#DDF3DE' }} className="text-black">
-      <div className="container mx-auto px-6 py-3 flex items-center justify-between">
+    <div style={{ backgroundColor: '#DDF3DE' }} className="text-black font-poppins">
+      <div className="container mx-auto px-6 py-3 flex items-center justify-between" style={{ paddingTop: '51px', paddingLeft: '67px' }}>
         <div className="flex items-center">
           <img src={logo} alt="logo" className="h-10 w-10 mr-3 rounded-full" />
-          <div className="text-xl font-bold" style={{ color: '#318A33' }}>REMINDER ME</div>
+          <img src={ReminderMelogo} alt="logo"/>
         </div>
         <div className="hidden md:flex space-x-3">
-          <a href="#" className="px-3 py-2">Home</a>
-          <a href="#" className="px-3 py-2">About</a>
-          <a href="#" className="px-3 py-2">Contact</a>
+          {['Home', 'About', 'Contact'].map(page => (
+            <a 
+              key={page}
+              href="#" 
+              className={`px-3 py-2 text-sm font-medium ${page === activePage ? 'text-#2F8331 border-b-2 border-#2F8331' : ''}`}
+              style={{ fontSize: '18px', fontWeight: '500' }}
+              onClick={() => setActivePage(page)}
+            >
+              {page}
+            </a>
+          ))}
         </div>
 
-        <div className="flex space-x-3">
-          <button className="px-4 py-2 border rounded-md">Login</button>
-          <button className="px-4 py-2 bg-black text-white rounded-md">Sign Up</button>
+        <div className="flex space-x-4">
+          <button 
+            style={{ 
+              borderColor: '#9F9F9F',
+              width: '126.58px',
+              height: '50.63px'
+            }}
+            className="border rounded-md bg-transparent text-black font-semibold"
+          >
+            Sign Up
+          </button>
+          <button 
+            style={{ 
+              background: 'linear-gradient(to right, #3DA93f, #2F8331)',
+              width: '126.58px',
+              height: '50.63px'
+            }}
+            className="rounded-md text-white font-semibold"
+          >
+            Log in
+          </button>
         </div>
 
         <div className="md:hidden">
@@ -31,11 +59,37 @@ function App() {
 
       {isOpen && (
         <div className="md:hidden">
-          <a href="#" className="block px-3 py-2">Home</a>
-          <a href="#" className="block px-3 py-2">About</a>
-          <a href="#" className="block px-3 py-2">Contact</a>
-          <button className="block px-4 py-2 border rounded-md">Login</button>
-          <button className="block px-4 py-2 bg-black text-white rounded-md">Sign Up</button>
+          {['Home', 'About', 'Contact'].map(page => (
+            <a 
+              key={page}
+              href="#" 
+              className={`block px-3 py-2 text-sm font-medium ${page === activePage ? 'text-#2F8331 border-b-2 border-#2F8331' : ''}`}
+              style={{ fontSize: '18px', fontWeight: '500' }}
+              onClick={() => setActivePage(page)}
+            >
+              {page}
+            </a>
+          ))}
+          <button 
+            style={{ 
+              borderColor: '#9F9F9F',
+              width: '126.58px',
+              height: '50.63px'
+            }}
+            className="block px-4 py-2 border rounded-md bg-transparent text-black font-semibold mt-3"
+          >
+            Sign Up
+          </button>
+          <button 
+            style={{ 
+              background: 'linear-gradient(to right, #3DA93f, #2F8331)',
+              width: '126.58px',
+              height: '50.63px'
+            }}
+            className="block px-4 py-2 rounded-md text-white font-semibold mt-3"
+          >
+            Login
+          </button>
         </div>
       )}
     </div>
@@ -43,4 +97,3 @@ function App() {
 }
 
 export default App;
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,4 +6,27 @@ module.exports = {
   },
   plugins: [],
 }
-
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      }
+    }
+  }
+}
+module.exports = {
+  theme: {
+    extend: {
+      backgroundColor: {
+        'nav-green': '#DDF3DE'
+      },
+      textColor: {
+        'reminder-green': '#318A33',
+        'active-nav': '#2F8331'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
