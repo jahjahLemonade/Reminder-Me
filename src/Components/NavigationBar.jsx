00f60d433907e1ffe97219a4cbe39a9cf79a21b5
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-09-08 17:52:33 +0100

    commit

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -7,19 +7,18 @@ function App() {
   const [activePage, setActivePage] = React.useState('Home'); // For demo purposes
 
   return (
-    <div style={{ backgroundColor: '#DDF3DE' }} className="text-black font-poppins">
+    <div className="bg-[#DDF3DE] text-black font-poppins">
       <div className="container mx-auto px-6 py-3 flex items-center justify-between" style={{ paddingTop: '51px', paddingLeft: '67px' }}>
         <div className="flex items-center">
           <img src={logo} alt="logo" className="h-10 w-10 mr-3 rounded-full" />
-          <img src={ReminderMelogo} alt="logo"/>
+          <img src={ReminderMelogo} alt="logo" />
         </div>
         <div className="hidden md:flex space-x-3">
           {['Home', 'About', 'Contact'].map(page => (
             <a 
               key={page}
               href="#" 
-              className={`px-3 py-2 text-sm font-medium ${page === activePage ? 'text-#2F8331 border-b-2 border-#2F8331' : ''}`}
-              style={{ fontSize: '18px', fontWeight: '500' }}
+              className={`px-3 py-2 text-[18px] font-medium ${page === activePage ? 'text-[#2F8331] border-b-2 border-[#2F8331]' : 'text-black'}`}
               onClick={() => setActivePage(page)}
             >
               {page}
@@ -29,24 +28,14 @@ function App() {
 
         <div className="flex space-x-4">
           <button 
-            style={{ 
-              borderColor: '#9F9F9F',
-              width: '126.58px',
-              height: '50.63px'
-            }}
-            className="border rounded-md bg-transparent text-black font-semibold"
+            className="border border-[#9F9F9F] rounded-md bg-transparent text-black font-semibold text-[14px] w-[126.58px] h-[50.63px]"
           >
             Sign Up
           </button>
           <button 
-            style={{ 
-              background: 'linear-gradient(to right, #3DA93f, #2F8331)',
-              width: '126.58px',
-              height: '50.63px'
-            }}
-            className="rounded-md text-white font-semibold"
+            className="rounded-md text-white font-semibold text-[14px] bg-gradient-to-r from-[#3DA93f] to-[#2F8331] w-[126.58px] h-[50.63px]"
           >
-            Log in
+            Login
           </button>
         </div>
 
@@ -63,30 +52,19 @@ function App() {
             <a 
               key={page}
               href="#" 
-              className={`block px-3 py-2 text-sm font-medium ${page === activePage ? 'text-#2F8331 border-b-2 border-#2F8331' : ''}`}
-              style={{ fontSize: '18px', fontWeight: '500' }}
+              className={`block px-3 py-2 text-[18px] font-medium ${page === activePage ? 'text-[#2F8331] border-b-2 border-[#2F8331]' : 'text-black'}`}
               onClick={() => setActivePage(page)}
             >
               {page}
             </a>
           ))}
           <button 
-            style={{ 
-              borderColor: '#9F9F9F',
-              width: '126.58px',
-              height: '50.63px'
-            }}
-            className="block px-4 py-2 border rounded-md bg-transparent text-black font-semibold mt-3"
+            className="block px-4 py-2 mt-3 border border-[#9F9F9F] rounded-md bg-transparent text-black font-semibold text-[14px] w-[126.58px] h-[50.63px]"
           >
             Sign Up
           </button>
           <button 
-            style={{ 
-              background: 'linear-gradient(to right, #3DA93f, #2F8331)',
-              width: '126.58px',
-              height: '50.63px'
-            }}
-            className="block px-4 py-2 rounded-md text-white font-semibold mt-3"
+            className="block px-4 py-2 mt-3 rounded-md text-white font-semibold text-[14px] bg-gradient-to-r from-[#3DA93f] to-[#2F8331] w-[126.58px] h-[50.63px]"
           >
             Login
           </button>
