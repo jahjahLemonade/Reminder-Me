91b8efe4c87620c67a406f2579cb1bb978193b7d
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-09-09 18:40:32 +0100

    commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,13 @@
+import logoInvert from '../images/footer/logoInvert.png';
+import note from '../images/footer/fluent_note-28-filled.png';
+import github_logo from '../images/footer/akar-icons_github-fill.png';
+import linked_logo from '../images/footer/bi_linkedin.png';
+
 function App() {
     return (
     <div className="w-[1440px] h-[90px] pl-[67px] pr-[63px] py-[25px] bg-green-700 justify-end items-start gap-[937px] inline-flex">
         <div className="self-stretch justify-start items-center gap-2.5 inline-flex">
-            <div className="w-10 h-10 relative shadow" />
+            <img src={logoInvert} alt="logo" className="w-10 h-10 relative shadow"/>
             <div className="text-white text-xl font-bold">REMINDER ME</div>
         </div>
         <div className="w-[182px] justify-center items-start gap-10 inline-flex">
