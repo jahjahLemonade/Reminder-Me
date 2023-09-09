ab74d8703821a1fedff6311833d4cd14e4c87f61
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-09-09 22:29:48 +0100

    commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,21 +7,23 @@ import linked_in_logo from '../images/footer/bi_linkedin.png';
 function App() {
     return (
     <div className="bg-green-700 text-white px-4 md:px-16 lg:pl-[67px] lg:pr-[63px] py-2 md:py-4 lg:py-[25px] flex items-center justify-between">
-        <div className="flex items-center space-x-2.5">
-            <a href="#" className="block">
-                <img src={logoInvert} alt="logo" className="w-6 h-6 md:w-8 md:h-8 lg:w-[34px] lg:h-[34px] relative shadow"/>
-            </a>
-            <a href="#" className="hidden md:block text-xl font-bold">REMINDER ME</a>
+        <div className="flex items-center space-x-2.5 lg:space-x-[168px]">
+            <div className="flex items-center space-x-2">
+                <a href="#" target="_blank" className="block">
+                    <img src={logoInvert} alt="logo" className="w-6 h-6 md:w-8 md:h-8 lg:w-[62px] lg:h-[62px] "/>
+                </a>
+                <a href="#" target="_blank" className="md:block text-xl font-bold">REMINDER ME</a>
+            </div>
         </div>
         <div className="flex space-x-2 md:space-x-4 lg:space-x-10">
-            <a href="#" className="block">
-                <img src={github_logo} alt="github_logo" className="w-6 h-6 md:w-8 md:h-8 lg:w-[34px] lg:h-[34px] relative"/>
+            <a href="#" target="_blank" className="block">
+                <img src={github_logo} alt="github_logo" className="w-6 h-6 md:w-8 md:h-8 lg:w-[34px] lg:h-[34px]"/>
             </a>
-            <a href="#" className="block">
-                <img src={linked_in_logo} alt="linked_in_logo" className="w-6 h-6 md:w-8 md:h-8 lg:w-[34px] lg:h-[34px] relative"/>
+            <a href="#" target="_blank" className="block">
+                <img src={linked_in_logo} alt="linked_in_logo" className="w-6 h-6 md:w-8 md:h-8 lg:w-[34px] lg:h-[34px]"/>
             </a>
-            <a href="#" className="block">
-                <img src={note} alt="note_logo" className="w-6 h-6 md:w-8 md:h-8 lg:w-[34px] lg:h-[34px] relative"/>
+            <a href="#" target="_blank" className="block">
+                <img src={note} alt="note_logo" className="w-6 h-6 md:w-8 md:h-8 lg:w-[34px] lg:h-[34px]"/>
             </a>
         </div>
     </div>
@@ -29,6 +31,3 @@ function App() {
 }
 
 export default App;
-
-
-
